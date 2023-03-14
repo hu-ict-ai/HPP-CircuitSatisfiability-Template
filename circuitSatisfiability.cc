161cf54d2b911af247726feffd2183c4be537ba5
Brian van der Bijl <peikos@peikos.net>
2023-03-14 23:00:00 +0100

    Modernised version

--- circuitSatisfiability.cc
+++ circuitSatisfiability.cc
@@ -9,38 +9,44 @@
  *   Extended to 32 bits by Joel C. Adams, Sept 2013.
  *   Adapted for HU-HPP by Huib Aldewereld, 2020.
  *   Adapted for C++ by Stephan van der Feest, 2022.
+ *   Modernised by Brian van der Bijl, 2023.
+ *     - Removed raw array, macros
  */
 
 #include <iostream>
 #include <climits>
+#include <array>
 
 using std::cout, std::endl;
 
-#define SIZE 32
+constexpr unsigned int SIZE = 32;
 
-/** EXTRACT_BIT is a macro that extracts the ith bit of number n.
+/** EXTRACT_BIT is a compiletime function that extracts the ith bit of number n.
  *
  * parameters: n, a number;
  *             i, the position of the bit we want to know.
  *
  * return: 1 if 'i'th bit of 'n' is 1; 0 otherwise
  */
-#define EXTRACT_BIT(n, i) ((n & (1 << i)) ? 1 : 0)
 
-int checkCircuit(int, bool *);
+inline constexpr bool EXTRACT_BIT(unsigned int n, unsigned int i) {
+  return (n & (1 << i));
+}
 
-int main (int argc, char *argv[])
-{
+typedef std::array<bool, SIZE> input;
+
+int checkCircuit(int, input&);
+
+int main (int argc, char *argv[]) {
    unsigned int i, combination; // loop variable (32 bits)
    int id = -1;                 // process id
-   int count = 0;        // number of solutions
+   int count = 0;               // number of solutions
 
-   bool *v = (bool *)malloc(sizeof(bool) * SIZE); /* Each element is one of the 32 bits */
+   input v;
 
    cout << endl << "Process " << id << " is checking the circuit..." << endl;
 
-   for (combination = 0; combination < UINT_MAX; combination++)
-   {
+   for (combination = 0; combination < UINT_MAX; combination++) {
       for (i = 0; i < SIZE; i++)
          v[i] = EXTRACT_BIT(combination, i);
 
@@ -59,33 +65,34 @@ int main (int argc, char *argv[])
  * output: the binary rep. of bits if the circuit outputs 1
  * return: 1 if the circuit outputs 1; 0 otherwise.
  */
-int checkCircuit(int id, bool *v)
-{
-   if ( ( (v[0] || v[1]) && (!v[1] || !v[3]) && (v[2] || v[3])
-       && (!v[3] || !v[4]) && (v[4] || !v[5])
-       && (v[5] || !v[6]) && (v[5] || v[6])
-       && (v[6] || !v[15]) && (v[7] || !v[8])
-       && (!v[7] || !v[13]) && (v[8] || v[9])
-       && (v[8] || !v[9]) && (!v[9] || !v[10])
-       && (v[9] || v[11]) && (v[10] || v[11])
-       && (v[12] || v[13]) && (v[13] || !v[14])
-       && (v[14] || v[15]) )
+int checkCircuit(int id, input& v) {
+   if ( ((v[0] || v[1]) && (!v[1] || !v[3]) && (v[2] || v[3])
+           && (!v[3] || !v[4]) && (v[4] || !v[5])
+           && (v[5] || !v[6]) && (v[5] || v[6])
+           && (v[6] || !v[15]) && (v[7] || !v[8])
+           && (!v[7] || !v[13]) && (v[8] || v[9])
+           && (v[8] || !v[9]) && (!v[9] || !v[10])
+           && (v[9] || v[11]) && (v[10] || v[11])
+           && (v[12] || v[13]) && (v[13] || !v[14])
+           && (v[14] || v[15]))
        &&
-          ( (v[16] || v[17]) && (!v[17] || !v[19]) && (v[18] || v[19])
-       && (!v[19] || !v[20]) && (v[20] || !v[21])
-       && (v[21] || !v[22]) && (v[21] || v[22])
-       && (v[22] || !v[31]) && (v[23] || !v[24])
-       && (!v[23] || !v[29]) && (v[24] || v[25])
-       && (v[24] || !v[25]) && (!v[25] || !v[26])
-       && (v[25] || v[27]) && (v[26] || v[27])
-       && (v[28] || v[29]) && (v[29] || !v[30])
-       && (v[30] || v[31]) ) )
+          ((v[16] || v[17]) && (!v[17] || !v[19]) && (v[18] || v[19])
+           && (!v[19] || !v[20]) && (v[20] || !v[21])
+           && (v[21] || !v[22]) && (v[21] || v[22])
+           && (v[22] || !v[31]) && (v[23] || !v[24])
+           && (!v[23] || !v[29]) && (v[24] || v[25])
+           && (v[24] || !v[25]) && (!v[25] || !v[26])
+           && (v[25] || v[27]) && (v[26] || v[27])
+           && (v[28] || v[29]) && (v[29] || !v[30])
+           && (v[30] || v[31]))
+      )
    {
-      std::printf("%d) %d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d \n", id,
-             v[31], v[30], v[29], v[28], v[27], v[26], v[25], v[24], v[23], v[22],
-             v[21], v[20], v[19], v[18], v[17], v[16], v[15], v[14], v[13], v[12],
-             v[11], v[10], v[9], v[8], v[7], v[6], v[5], v[4], v[3], v[2], v[1], v[0]);
-      fflush(stdout);
+     cout << "(" << id << ") "
+       << v[31] << v[30] << v[29] << v[28] << v[27] << v[26] << v[25] << v[24]
+       << v[23] << v[22] << v[21] << v[20] << v[19] << v[18] << v[17] << v[16]
+       << v[15] << v[14] << v[13] << v[12] << v[11] << v[10] << v[ 9] << v[ 8]
+       << v[ 7] << v[ 6] << v[ 5] << v[ 4] << v[ 3] << v[ 2] << v[ 1] << v[ 0]
+       << endl;
       return 1;
    } else {
       return 0;
